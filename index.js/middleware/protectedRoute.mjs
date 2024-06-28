bb6d5245426a08d34d4cf191d7c5d4b35b590965
michee2x <157503371+michee2x@users.noreply.github.com>
2024-06-28 23:04:28 +0100

    Update protectedRoute.mjs

--- index.js/middleware/protectedRoute.mjs
+++ index.js/middleware/protectedRoute.mjs
@@ -3,7 +3,7 @@ import User from "../models/user.model.mjs"
 
 export const protectedRoute = async (req, res, next) => {
 try{
-    console.log("this is the header",req.headers.cookie, "this is the cookie", req.cookies)
+    console.log("this is the header",req.headers.cookie, "this is the cookie", req.cookies.jwtToken)
     const cookies = req.cookie.jwtToken
     
     if(!cookies) {
