2e591b8bb216a64b88da06730400dc846693cffe
michee2x <157503371+michee2x@users.noreply.github.com>
2024-06-28 03:45:07 +0100

    Update server.mjs

--- index.js/server.mjs
+++ index.js/server.mjs
@@ -40,7 +40,7 @@ const imageUpload = multer({storage:imageStorage})
 
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
-app.use(cors({credentials:true, origin:"http://localhost:5173"}))
+app.use(cors({credentials:true, origin:"https://chat-x-backend.onrender.com"}))
 app.use("/api/post/createpost",protectedRoute, imageUpload.single("file"), createPost)
 app.use(express.json())
 
