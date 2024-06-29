b445d26cf2122c740221920e05ced0b2bebafb35
michee2x <157503371+michee2x@users.noreply.github.com>
2024-06-29 01:05:05 +0100

    Update fetchData.ts

--- codegram/src/fetchData.ts
+++ codegram/src/fetchData.ts
@@ -1,7 +1,7 @@
 
 export  const fetchData = async (setPost:any, setLoading:any=true, page:number=0) => {
       try{
-        const res = await fetch(`http://localhost:7000/api/post/getposts?page=${page}`,{
+        const res = await fetch(`https://chat-x-backend.onrender.com/api/post/getposts?page=${page}`,{
           credentials:'include'
         }) 
         const data = await res.json()
