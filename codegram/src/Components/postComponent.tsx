9ad8619195b24f13a176f9540ae36df61d4bc10b
michee2x <157503371+michee2x@users.noreply.github.com>
2024-06-30 02:50:17 +0100

    Update postComponent.tsx

--- codegram/src/Components/postComponent.tsx
+++ codegram/src/Components/postComponent.tsx
@@ -75,14 +75,14 @@ const PostComponent = ({
                     <div className="w-full max-h-64 rounded-tl-2xl rounded-tr-xl h-auto lg:w-[70%]">
                       {e.file?.split("/")[4] === "video" ? (
                         <div className="w-full relative h-auto">
-                          <div className="w-full h-full grid place-items-center hidden absolute">
+                          <div className="w-full h-full grid place-items-center absolute">
                             <span className="w-14 h-14 bg-blue-700 flex items-center justify-center rounded-full z-10">
                               <FaPlay className="text-[1.5rem]" />
                             </span>
                           </div>
                           <video
                             src={e.file}
-                            className={`w-full max-h-64 ${
+                            className={`w-full h-auto ${
                               from === "bookmark"
                                 ? "rounded-2xl"
                                 : "rounded-tr-2xl rounded-tl-2xl"
