6cb2305cecfa54d3fe31696225ccbd11ed8da0d2
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-02 22:40:20 +0100

    Update Home.tsx

--- codegram/src/Components/Home.tsx
+++ codegram/src/Components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useRef, useState } from "react";
+import { useEffect,useRef, useState } from "react";
 import { fetchData } from "../fetchData";
 import { MdPerson } from 'react-icons/md';
 import {Navigate} from "react-router-dom";
