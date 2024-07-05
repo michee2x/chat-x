5e2ca24f317c67a5aa25832bde96dca93fa7aefb
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-05 11:05:43 +0100

    Update postComponent.tsx

--- codegram/src/Components/postComponent.tsx
+++ codegram/src/Components/postComponent.tsx
@@ -138,11 +138,11 @@ console.log(like)
               <span className="text-xs">{e?.bookmark.length}</span>
             </div>
 <div
-              className={`w-12 h-full rounded-full cursor-pointer gap-2 flex items-center justify-center text-xm font-bold`}
+              className={`w-12 h-full rounded-full cursor-pointer gap-2 flex items-center text-white justify-center text-xm font-bold`}
             >
               <span>
                 <FaComment
-                  className={`text-sm text-white border-[1px] border-white cursor-pointer`}
+                  className={`text-sm`}
                 />
                
               </span>
