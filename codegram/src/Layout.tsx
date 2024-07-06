c1c530883aa3aa370f52253e47080c2f74a395dc
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-06 20:04:36 +0100

    Update Layout.tsx

--- codegram/src/Layout.tsx
+++ codegram/src/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './Components/sidebar';
 import RecomendedUsers from './Components/RecomendedUsers';
 import { SideBarContext } from "./showSideBar";
-import {MdMenu} from "react-icons/md"
+import {FaPlus} from "react-icons/fa"
 
 const Layout = () => {
   const { showSideBar, setshowSideBar } = SideBarContext();
@@ -11,10 +11,9 @@ const Layout = () => {
       <div className="flex relative overflow-y-hidden w-screen h-screen">
         <Sidebar/>
         <div
-          className="w-14 hidden h-14 z-40 rounded-full bg-blue-600 border-2 border-white fixed grid place-items-center lg:hidden"
-          style={{ top: "10%", left: "1.2%" }}
+          className="w-16 h-16 z-40 rounded-full bg-blue-600 bottom-10 right-10 border-2 border-white fixed grid place-items-center lg:hidden"
           onClick={() => setshowSideBar(true)}
-        ><MdMenu className="text-[2rem] text-white/80" /></div>
+        ><FaPlus className="text-[2rem] text-white/80" /></div>
         <div onClick={() => setshowSideBar(false)} className={`w-screen absolute ${showSideBar ? "block bg-black" : "hidden"} h-screen bg-transparent z-40`}></div>
       
         <div className="bg-gray-300" style={{ width: "70%" }}>
