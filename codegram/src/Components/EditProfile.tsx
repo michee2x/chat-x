59b2f93973ad15d76d5befb2d4a6d78c70e9dea9
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-07 15:32:39 +0100

    Update EditProfile.tsx

--- codegram/src/Components/EditProfile.tsx
+++ codegram/src/Components/EditProfile.tsx
@@ -5,6 +5,7 @@ import {editProfile, getloggedUser } from "../hooks/likepost";
 import { MdArrowBack, MdCancel } from "react-icons/md";
 
 const EditProfile = () => {
+const [typing, setTyping] = useState(false)
 const [user, setUser] = useState<any>({})
 const [cp,setCp] = useState(false)
  const [editing, setEditing] = useState(false)
@@ -137,8 +138,9 @@ setTimeout(() => {
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <input
-            value={profile.name ? profile.name : user?.name}
+            value={typing && profile.name ? profile.name : user?.name}
             onChange={(e) => setProfile({ ...profile, name: e.target.value })}
+            onFocus={() => setTyping(true)}
             type="text"
             spellCheck="false"
             className="grow text-white"
@@ -155,11 +157,12 @@ setTimeout(() => {
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <input
-            value={profile.username ? profile.username : user?.username}
+            value={typing && profile.username ? profile.username : user?.username}
             onChange={(e) =>
               setProfile({ ...profile, username: e.target.value })
             }
             type="text"
+onFocus={() => setTyping(true)}
             spellCheck="false"
             className="grow text-white"
             placeholder="username"
@@ -176,8 +179,9 @@ setTimeout(() => {
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
           <input
-            value={profile.email ? profile.email : user?.email}
+            value={typing && profile.email ? profile.email : user?.email}
             onChange={(e) => setProfile({ ...profile, email: e.target.value })}
+onFocus={() => setTyping(true)}
             type="email"
             className="grow text-white"
             placeholder="email"
