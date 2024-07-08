bd2fd606952add04af385b794a5f33d1c569635f
michee2x <157503371+michee2x@users.noreply.github.com>
2024-07-08 04:29:42 +0100

    Update postComponent.tsx

--- codegram/src/Components/postComponent.tsx
+++ codegram/src/Components/postComponent.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {Link} from "react-router-dom"
 import { FaBookmark, FaHeart, FaComment, FaPlay } from "react-icons/fa";
 import {BsPerson } from "react-icons/bs";
 import {
